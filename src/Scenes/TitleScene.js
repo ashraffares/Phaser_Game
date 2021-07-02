15d395dd2de6e739d38cd91b4853c7526bbcd921
ashraffares <ashraffares090@gmail.com>
2021-07-02 10:54:26 +0200

    Create and display options button and text

--- src/Scenes/TitleScene.js
+++ src/Scenes/TitleScene.js
@@ -39,6 +39,17 @@ export default class TitleScene extends Phaser.Scene {
     );
   }
 
+    // Options
+    this.optionsButton = this.add.sprite(300, 200, 'blueButton1').setInteractive();
+    this.centerButton(this.optionsButton);
+    
+    this.optionsText = this.add.text(0, 0, 'Options', { fontSize: '32px', fill: '#fff' });
+    this.centerButtonText(this.optionsText, this.optionsButton);
+    
+    this.optionsButton.on('pointerdown', function (pointer) {
+    this.scene.start('Options');
+    }.bind(this));
+
   centerButtonText(gameText, gameButton) {
     Phaser.Display.Align.In.Center(
       gameText,
