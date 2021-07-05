df113005a8bd774b9131b22455174a0fa75ea739
ashraffares <ashraffares090@gmail.com>
2021-07-05 11:41:01 +0200

    Update config center the game and use arcade

--- src/Config/config.js
+++ src/Config/config.js
@@ -2,7 +2,17 @@ import Phaser from 'phaser';
 
 export default {
   type: Phaser.AUTO,
-  parent: 'phaser-example',
+  mode: Phaser.Scale.FIT,
+  parent: 'Shooter Game',
+  autoCenter: Phaser.Scale.CENTER_BOTH,
   width: 800,
   height: 600,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { x: 0, y: 0 },
+    },
+  },
+  pixelArt: true,
+  roundPixels: true,
 };
