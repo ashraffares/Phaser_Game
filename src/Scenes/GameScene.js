4c7395c76edcab9fe04e1a70e48675e8c0a0c216
ashraffares <ashraffares090@gmail.com>
2021-07-05 22:35:05 +0200

    Add entity.js - player.js and fully implement the player movement

--- src/Scenes/GameScene.js
+++ src/Scenes/GameScene.js
@@ -1,5 +1,6 @@
 import Phaser from 'phaser';
-import logo from '../assets/logo.png';
+import sprPlayer from '../assets/Shooter/sprPlayer.png';
+import Player from '../Entity/player';
 
 export default class GameScene extends Phaser.Scene {
   constructor() {
@@ -8,10 +9,47 @@ export default class GameScene extends Phaser.Scene {
 
   preload() {
     // load images
-    this.load.image('logo', logo);
+    this.load.spritesheet('sprPlayer', sprPlayer, {
+      frameWidth: 16,
+      frameHeight: 16,
+    });
   }
 
   create() {
-    this.add.image(400, 300, 'logo');
+    this.player = new Player(
+      this,
+      this.game.config.width * 0.5,
+      this.game.config.height * 0.5,
+      'sprPlayer',
+    );
+
+    this.anims.create({
+      key: 'sprPlayer',
+      frames: this.anims.generateFrameNumbers('sprPlayer'),
+      frameRate: 20,
+      repeat: -1,
+    });
+
+    this.keyW = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+    this.keyS = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+    this.keyA = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+    this.keyD = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+    this.keySpace = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+  }
+
+  update() {
+    this.player.update();
+
+    if (this.keyW.isDown) {
+      this.player.moveUp();
+    } else if (this.keyS.isDown) {
+      this.player.moveDown();
+    }
+
+    if (this.keyA.isDown) {
+      this.player.moveLeft();
+    } else if (this.keyD.isDown) {
+      this.player.moveRight();
+    }
   }
 }
