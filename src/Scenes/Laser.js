204cc5c368e0a23e3e5506fbb60cfa97664d52f6
ashraffares <ashraffares090@gmail.com>
2021-07-06 22:28:36 +0200

    Change assets images and sound

---
+++ src/Scenes/Laser.js
@@ -0,0 +1,14 @@
+import Phaser from 'phaser';
+
+export default class Laser extends Phaser.Physics.Arcade.Sprite {
+  constructor(scene, x, y) {
+    super(scene, x, y, 'laser');
+  }
+
+  fire(x, y) {
+    this.body.reset(x, y);
+    this.setActive(true);
+    this.setVisible(true);
+    this.setVelocityY(-900);
+  }
+}
